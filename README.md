dd35164deed6b4a865ad697cb78d4436afecfc1a
barakadax <barakadax@gmail.com>
2023-04-05 11:17:34 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nunit debugging extensions
+
+Some tools I need to debug my code with using the <a href="https://github.com/nunit/nunit" target="_blank">nunit</a> package.
---
+++ Test extensions/RetryUntilFailureAttributeTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using nunit_debugging_extensions;
+
+namespace Test_extensions;
+
+[TestFixture]
+public class RetryUntilFailureAttributeFixture
+{
+    private int _counter;
+
+    [Test, RetryUntilFailure, Ignore("Test should fail when run")]
+    public void RunUntilFailure_ShouldFail()
+    {
+        // Act + Assert
+        Assert.IsTrue(_counter <= 10);
+        _counter++;
+    }
+
+    [Test, RetryUntilFailure(20), Ignore("Test should fail when run")]
+    public void HitFailureBeforeMaxRetries_ShouldFail()
+    {
+        // Act + Assert
+        Assert.IsTrue(_counter <= 10);
+        _counter++;
+    }
+
+    [Test, RetryUntilFailure(5)]
+    public void HitMaxRetriesBeforeFailure_ShouldPass()
+    {
+        // Act + Assert
+        Assert.IsTrue(_counter <= 10);
+        _counter++;
+    }
+}
